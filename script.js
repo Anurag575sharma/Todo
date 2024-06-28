73f3c0b01482919078520d48b90b55c872af069e
Anurag sharma <anuragsharma575dta@gmail.com>
2024-06-28 13:49:30 +0530

    My Todo

---
+++ script.js
@@ -0,0 +1,30 @@
+let todoList = [];
+displayTask();
+function addTodo() {
+  let todoElement = document.querySelector("#todo-input");
+  let todoDate = document.querySelector("#item-date");
+  let todoItem = todoElement.value;
+  let todoTime = todoDate.value;
+  todoList.push({ item: todoItem, dueDate: todoTime });
+  todoElement.value = "";
+  todoDate.value = "";
+
+  displayTask();
+}
+
+function displayTask() {
+  let displayElement = document.querySelector(".container-items");
+  let newHtml = "";
+  for (let i = 0; i < todoList.length; i++) {
+    let { item, dueDate } = todoList[i];
+    if (item === "" && dueDate == "") {
+      continue;
+    } // skip empty items and undefined dates
+    newHtml += `
+    <span>${item} 
+      </span>
+      <span>${dueDate} 
+      </span> <button id="todo-delete" onclick="todoList.splice(${i},1); displayTask()">Delete</button>`;
+  }
+  displayElement.innerHTML = newHtml;
+}
